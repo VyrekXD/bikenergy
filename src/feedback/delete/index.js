13575930756b5a9726f7b2e6dd4c97ee21091d79
VyrekXD <vyrekxd@gmail.com>
2022-11-28 14:19:24 +0000

    update

--- src/feedback/delete/index.js
+++ src/feedback/delete/index.js
@@ -1,7 +1,13 @@
-window.addEventListener('load', async () => {
-	await fetch('https://back-bikenergy.vyrek.dev/delete', {
-		headers: {
-			authorization: JSON.stringify({ password: 'bikenergy2022' })
-		}
+window.addEventListener('load', () => {
+	const password = document.getElementById('password')
+	const button = document.getElementById('send-delete')
+
+	button.addEventListener('click', async () => {
+		await fetch('https://back-bikenergy.vyrek.dev/delete', {
+			headers: {
+				authorization: JSON.stringify({ password })
+			}
+		})
 	})
 })
+
